6170df660934de8fed3accb58428b67addceb79c
toanngosy <toanngosy@gmail.com>
2022-12-05 16:23:28 -0800

    update tests

--- tests/argparse/test_argparse.py
+++ tests/argparse/test_argparse.py
@@ -1,8 +1,10 @@
 import unittest
 from unittest.mock import patch
 import sys
+import yaml
 import oneflux
 from oneflux.configs.utils import ONEFluxConfig, argparse_from_yaml_and_cli
+from oneflux.tools.partition_nt import PROD_TO_COMPARE, PERC_TO_COMPARE
 
 class TestArgParse(unittest.TestCase):
     def test_argparse_default(self):
@@ -69,3 +71,44 @@ class TestArgParse(unittest.TestCase):
                                 "--configfile", f"{test_yaml_path}"]):
             with self.assertRaises(ValueError):
                 _, _, args = argparse_from_yaml_and_cli()
+
+    def test_onefluxconfig_args(self):
+        test_yaml_path = './tests/argparse/config.yaml'
+        with patch("sys.argv", ["python runoneflux.py",
+                                "--configfile", f"{test_yaml_path}"]):
+            onefluxconfig = ONEFluxConfig()
+        self.assertEqual(onefluxconfig.perc, PERC_TO_COMPARE)
+        self.assertEqual(onefluxconfig.prod, PROD_TO_COMPARE)
+
+    def test_onefluxconfig_export_to_yaml(self):
+        dir = './tests/argparse'
+        yaml_filename = 'config.yaml'
+        output_yaml_filename = 'output_config.yaml'
+        test_yaml_path = f'{dir}/{yaml_filename}'
+        test_output_yaml_path = f'{dir}/{output_yaml_filename}'
+        with patch("sys.argv", ["python runoneflux.py",
+                                "--configfile", test_yaml_path]):
+            onefluxconfig = ONEFluxConfig()
+            onefluxconfig.export_to_yaml(dir,
+                                         output_yaml_filename,
+                                         overwritten=True)
+        with open(test_output_yaml_path) as f:
+            conf = yaml.safe_load(f)
+        self.assertEqual(conf['run']['configfile'], onefluxconfig.args.configfile)
+        self.assertEqual(conf['run']['era_fy'], onefluxconfig.args.erafy)
+        self.assertEqual(conf['run']['era_ly'], onefluxconfig.args.eraly)
+        self.assertEqual(conf['run']['mcr'], onefluxconfig.args.mcr_directory)
+        self.assertEqual(conf['run']['sitedir'], onefluxconfig.args.sitedir)
+        self.assertEqual(conf['run']['siteid'], onefluxconfig.args.siteid)
+        self.assertEqual(conf['run']['versiond'], onefluxconfig.args.versiond)
+        self.assertEqual(conf['run']['versionp'], onefluxconfig.args.versionp)
+
+        with patch("sys.argv", ["python runoneflux.py",
+                                "--configfile", test_output_yaml_path]):
+            new_onefluxconfig = ONEFluxConfig()
+            
+        no_check_args_list = ['timestamp', 'configfile']
+        for name, val in onefluxconfig.args._get_kwargs():
+            if name not in no_check_args_list:
+                self.assertEqual(val,
+                                 new_onefluxconfig.args.__getattribute__(name))
