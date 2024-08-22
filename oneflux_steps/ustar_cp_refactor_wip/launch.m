40408597d1748fbbf99669596288e6158bea49e2
James Emberton <jamie.emberton@gmail.com>
2024-08-22 09:15:55 +0100

    first regression and unit tests for launch.m

--- oneflux_steps/ustar_cp_refactor_wip/launch.m
+++ oneflux_steps/ustar_cp_refactor_wip/launch.m
@@ -12,26 +12,26 @@ warning off;
 
 % check input path
 if 0 == exist('input_folder')
-    input_folder = [pwd '\'];
+    input_folder = [pwd '/'];
 elseif length(input_folder) < 2
     if input_folder(2) ~= ':'
-        input_folder = [pwd '\' input_folder];
+        input_folder = [pwd '/' input_folder];
     end
 end
 if input_folder(length(input_folder)) ~= '\' && input_folder(length(input_folder)) ~= '/'
-    input_folder = [input_folder '\'];
+    input_folder = [input_folder '/'];
 end
 
 % check output path
 if 0 == exist('output_folder')
-    output_folder = [pwd '\'];
+    output_folder = [pwd '/'];
 elseif length(output_folder) < 2 
     if output_folder(2) ~= ':'
-        output_folder = [pwd '\' output_folder];  
+        output_folder = [pwd '/' output_folder];  
     end
 end
 if output_folder(length(output_folder)) ~= '\' && output_folder(length(output_folder)) ~= '/'
-    output_folder = [output_folder '\'];
+    output_folder = [output_folder '/'];
 end
 mkdir(output_folder);
 
