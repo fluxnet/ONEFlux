ad86606cd4bb2604374686fac7fa361777e11f9a
James Emberton <jamie.emberton@gmail.com>
2024-10-14 18:25:19 +0100

    deleted surplus code

--- oneflux_steps/ustar_cp_refactor_wip/cpdBootstrap_modules/filterValidData.m
+++
@@ -1,6 +0,0 @@
-function [itNee, ntNee] = filterValidData(NEE, uStar, T, iNight)
-    % Filter valid data based on non-NaN values and nighttime condition
-    itNee = find(~isnan(NEE + uStar + T));
-    itNee = intersect(itNee, iNight);
-    ntNee = length(itNee);
-end
--- oneflux_steps/ustar_cp_refactor_wip/cpdBootstrap_modules/initializeOutputs.m
+++
@@ -1,8 +0,0 @@
-function [Cp2, Cp3, Stats2, Stats3] = initializeOutputs(nSeasons, nStrataX, nBoot, StatsMT)
-    % Initialize output matrices
-    Cp2 = NaN * ones(nSeasons, nStrataX, nBoot);
-    Cp3 = NaN * ones(nSeasons, nStrataX, nBoot);
-    
-    Stats2 = repmat(StatsMT, [nSeasons, nStrataX, nBoot]);
-    Stats3 = repmat(StatsMT, [nSeasons, nStrataX, nBoot]);
-end
