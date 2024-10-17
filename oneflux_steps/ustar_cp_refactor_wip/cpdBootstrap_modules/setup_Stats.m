00420eb3192ec7a2b65e1089cf7046e29e6893ba
tztsai <1213815284@qq.com>
2024-10-17 11:00:54 +0100

    fix setup_Stats

--- oneflux_steps/ustar_cp_refactor_wip/cpdBootstrap_modules/setup_Stats.m
+++ oneflux_steps/ustar_cp_refactor_wip/cpdBootstrap_modules/setup_Stats.m
@@ -1,17 +1,14 @@
-function [Stats] = setup_Stats(nBoot, nSeasons, nStrataX, varargin)
+function [Stats] = setup_Stats(nBoot, nSeasons, nStrataX)
 
     StatsMT = generate_statsMT();
 
     Stats=StatsMT;
-	    for iBoot=1:nBoot;
-		    for iSeason=1:nSeasons;
-			    for iStrata=1:nStrataX;
-				    Stats(iSeason,iStrata,iBoot)=StatsMT;
-			    end; 
-		    end; 
-	    end;
-	if size(varargin)>0;
-		Stats = jsonencode(Stats);
-	end
 
+	for iBoot=1:nBoot;
+		for iSeason=1:nSeasons;
+			for iStrata=1:nStrataX;
+				Stats(iSeason,iStrata,iBoot)=StatsMT;
+			end; 
+		end; 
+	end;
 end
