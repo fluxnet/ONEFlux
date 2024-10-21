69a1415b1ed359130e5783aabd1c6b621d7ef608
tztsai <1213815284@qq.com>
2024-10-21 22:25:51 +0100

    updates conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -114,8 +114,8 @@ def matlab_engine(request, refactored=True):
     After the tests complete, the MATLAB engine is closed automatically.
     """
     if request.param == "translated":  # return the translated python module
-        module_name = "oneflux_steps.ustar_cp_python_auto"
-        yield importlib.import_module(module_name)
+        import oneflux_steps.ustar_cp_python_auto as eng
+        yield eng
         return
 
     # Start MATLAB engine
