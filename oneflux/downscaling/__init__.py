1a6f63b329040ee2e5fec014d8ab4fe57601bd9e
Gilberto Pastorello <gzpastorello@lbl.gov>
2024-11-05 17:29:12 +0100

    initial commit adding downscaling implementation from Carlo

---
+++ oneflux/downscaling/__init__.py
@@ -0,0 +1,12 @@
+'''
+oneflux.downscaling
+
+For license information:
+see LICENSE file or headers in oneflux.__init__.py
+
+Downscaling processing for gapfilling long gaps in meteo data
+
+@author: Gilberto Pastorello
+@contact: gzpastorello@lbl.gov
+@date: 2024-11-01
+'''
