c2f5df0f0d1682a2eef7dd009144153f58cb36c8
James Emberton <jamie.emberton@gmail.com>
2024-11-11 11:59:54 +0000

    modularised function

--- oneflux_steps/ustar_cp_refactor_wip/cpdFmax2pCp3_modules/calculate_p_interpolate.m
+++ oneflux_steps/ustar_cp_refactor_wip/cpdFmax2pCp3_modules/calculate_p_interpolate.m
@@ -1,3 +1,4 @@
 function p = calculate_p_interpolate(Fmax, FmaxCritical, pTable)
+    % calculate_p_interpolate calculates p using interpolation.
     p = interp1(FmaxCritical, 1 - pTable, Fmax, 'pchip');
 end
