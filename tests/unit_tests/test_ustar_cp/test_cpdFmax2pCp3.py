6c61ce3244816645d602acfb6e6bb63c0424af23
isaac <ia464@cam.ac.uk>
2024-11-12 18:25:38 +0000

    Tests for cpdFmax2pCp3 function

---
+++ tests/unit_tests/test_ustar_cp/test_cpdFmax2pCp3.py
@@ -0,0 +1,44 @@
+import matlab.engine
+import numpy as np
+import pytest
+
+
+testcases = [ 
+        # Test case 1: Input values are NaN
+        (np.nan, 52, np.nan),
+        (5, np.nan, np.nan),
+        
+        # Test case 2: Below f-critical(1)
+        (5.45204127574611, 52, 0.384643400326067),
+        # Test case 3: Above f-critical(3)
+        (12, 52, 0.0363307547269088),
+        # Test case 4: Between f-critical(1) and f-critical(3)
+        (10, 52, 0.0761404222166437),
+        # Test case 5: fmax = 0
+        (0, 52, 1),
+        # Test case 6: fmax = fcritical(1)
+        (1.6301, 52, 1),
+        # Test case 7: Other n-values
+        (2.37324492970613, 55, 1),
+        (10.3567400792636, 54, 0.0657053181314848)
+]
+@pytest.mark.parametrize("fmax, n, expected_p3", testcases)
+def test_cpdFmax2pCp3(matlab_engine, fmax, n, expected_p3):
+    """
+    Test the cpdFmax2pCp3 function in MATLAB.
+    Args:
+        matlab_engine (fixture): Fixture that initializes and manages the MATLAB engine session.
+        fmax, n : Matlab function input arguments
+        expected_p3 (float): The expected value of p3.
+    """
+    # Convert the input values to MATLAB compatible data types
+    fmax = matlab.double(fmax)
+    n = matlab.double(n)
+
+    # Run the MATLAB function
+    output_p3 = matlab_engine.cpdFmax2pCp3(fmax, n, nargout=1)
+
+
+    assert np.isclose(output_p3, expected_p3, equal_nan=True), f"Expected p3 value of {expected_p3}, but got {output_p3}."
+
+
