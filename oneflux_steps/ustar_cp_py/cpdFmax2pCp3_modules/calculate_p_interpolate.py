92a6376c01c8c577de3d36a0a4f9a7b12707c350
tztsai <1213815284@qq.com>
2024-12-03 10:52:23 +0000

    fix pytest arguments

--- oneflux_steps/ustar_cp_py/cpdFmax2pCp3_modules/calculate_p_interpolate.py
+++ oneflux_steps/ustar_cp_py/cpdFmax2pCp3_modules/calculate_p_interpolate.py
@@ -9,7 +9,7 @@ def calculate_p_interpolate(Fmax=None, FmaxCritical=None, pTable=None):
 
     # calculate_p_interpolate calculates p using interpolation.
     p = interp1(FmaxCritical, 1 - pTable, Fmax, "pchip")
-    # oneflux_steps/ustar_cp_refactor_wip/cpdFmax2pCp3_modules/calculate_p_interpolate.m:3
+    # oneflux_steps/ustar_cp_refactor_wip/cpdFmax2pCp3_modules/calculate_p_interpolate.m:2
     return p
 
 
