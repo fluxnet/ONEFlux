d3d545ad054394a1d36de6a81a94890db2eee66b
James Emberton <jamie.emberton@gmail.com>
2024-12-12 16:22:20 +0100

    tidied up original file

--- oneflux_steps/ustar_cp/fcReadFields.m
+++ oneflux_steps/ustar_cp/fcReadFields.m
@@ -1,39 +1,23 @@
-function x = fcReadFields(stats, FieldName, varargin)
-
-    % Initialize output
-    nd = ndims(s); 
-    ns = size(s); 
-    x = NaN * ones(ns); 
-    
-    % Handle 2D and 3D arrays
-    switch nd
-        case 2
-            for i = 1:ns(1)
-                for j = 1:ns(2)
-                    try
-                        tmp = getfield(s, {i, j}, FieldName); 
-                        if ~isempty(tmp)
-                            x(i, j) = tmp;
-                        end
-                    catch
-                        % Field not found
-                    end
-                end
-            end
-        case 3
-            for i = 1:ns(1)
-                for j = 1:ns(2)
-                    for k = 1:ns(3)
-                        try
-                            tmp = getfield(s, {i, j, k}, FieldName); 
-                            if ~isempty(tmp)
-                                x(i, j, k) = tmp;
-                            end
-                        catch
-                            % Field not found
-                        end
-                    end
-                end
-            end
-    end
-end
+	function x = fcReadFields(s,FieldName); 
+		
+	nd=ndims(s); ns=size(s); x=NaN*ones(ns); 
+	
+	switch nd; 
+		case 2; 
+			for i=1:ns(1); 
+				for j=1:ns(2); 
+					tmp=getfield(s,{i,j},FieldName); 
+					if ~isempty(tmp); x(i,j)=tmp; end;
+				end; 
+			end; 
+		case 3; 
+			for i=1:ns(1); 
+				for j=1:ns(2); 
+					for k=1:ns(3); 
+						tmp=getfield(s,{i,j,k},FieldName); 
+						if ~isempty(tmp); x(i,j,k)=tmp; end; 
+					end;
+				end;
+			end;
+		otherwise; 
+	end; 
