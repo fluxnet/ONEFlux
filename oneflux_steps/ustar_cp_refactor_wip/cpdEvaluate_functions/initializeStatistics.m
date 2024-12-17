b0e0abb5f51b8eac8817236da1e1e91c9ba6e8af
isaac <ia464@cam.ac.uk>
2024-12-17 19:13:37 +0000

    Replaced duplicate code within initialize statistics with an already created function

--- oneflux_steps/ustar_cp_refactor_wip/cpdEvaluate_functions/initializeStatistics.m
+++ oneflux_steps/ustar_cp_refactor_wip/cpdEvaluate_functions/initializeStatistics.m
@@ -1,12 +1,6 @@
 function [Stats2, Stats3] = initializeStatistics(nSeasons, nStrataX, varargin)
 
-	StatsMT=[]; 
-	StatsMT.n=NaN; StatsMT.Cp=NaN; StatsMT.Fmax=NaN; StatsMT.p=NaN; 
-	StatsMT.b0=NaN; StatsMT.b1=NaN; StatsMT.b2=NaN; StatsMT.c2=NaN; 
-	StatsMT.cib0=NaN; StatsMT.cib1=NaN; StatsMT.cic2=NaN; % preceding elements output by changepoint function
-	StatsMT.mt=NaN; StatsMT.ti=NaN; StatsMT.tf=NaN; 
-	StatsMT.ruStarVsT=NaN; StatsMT.puStarVsT=NaN; 
-	StatsMT.mT=NaN; StatsMT.ciT=NaN; % these elements added by this program.  
+	StatsMT = generate_statsMT();
 						
 	Stats2=StatsMT; Stats3=StatsMT; 
 	for iSeason=1:nSeasons;
