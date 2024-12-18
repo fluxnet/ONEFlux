21e00030c4d17e10d4841f431048f61cf17eeab5
Dominic Orchard <dom.orchard@gmail.com>
2024-12-18 21:00:15 +0000

    some comments

--- oneflux_steps/ustar_cp_refactor_wip/fcr2Calc.m
+++ oneflux_steps/ustar_cp_refactor_wip/fcr2Calc.m
@@ -1,10 +1,19 @@
-	function r2=r2Calc(y,yHat);    
+	function r2=fcr2Calc(y,yHat);    
    
+   % This function calculates the r^2 measure (coeffecient of determination)
+   % for a pair of data `y` and some predicted `yHat`.
+  
    n=length(y); ym=mean(y); 
    
+   % explained sum of squares
    SSreg=sum((yHat-ym).^2); 
+
+   % total sum of squares
    SStotal=sum((y-ym).^2); 
-   rmse=sum((yHat-y).^2); 
+
+   rmse=sum((yHat-y).^2);  % Note this is redundant
+
+   % coeffecient of determination
    r2=SSreg/SStotal; 
    
 % % %    sprintf('%7.4f %7.4f %7.4f ',[SSreg SStotal r2 rmse]) 
