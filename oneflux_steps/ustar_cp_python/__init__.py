e7f4f00619904c3409dc7760915fce3f4a6c7975
Dominic Orchard <dom.orchard@gmail.com>
2025-01-09 11:57:52 +0000

    slight tweak to __init__.py

--- oneflux_steps/ustar_cp_python/__init__.py
+++ oneflux_steps/ustar_cp_python/__init__.py
@@ -1,7 +1,7 @@
 # Import single-function-per-file modules
 from .fcx2colvec import fcx2colvec
 from .fcx2rowvec import fcx2rowvec
-from .cpdFmax2pCp3 import interpolate_FmaxCritical
+from .cpdFmax2pCp3 import interpolate_FmaxCritical, cpdFmax2pCp3
 
 from os.path import dirname, basename, isfile, join
 import glob
