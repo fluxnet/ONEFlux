ca026169c6f02527df3dec4ee2338359604eb5ea
Dominic Orchard <dom.orchard@gmail.com>
2025-01-16 20:36:55 +0000

    improve fcBin tests and handle overflow from generated tests

--- tests/unit_tests/test_ustar_cp/test_fcBin.py
+++ tests/unit_tests/test_ustar_cp/test_fcBin.py
@@ -29,6 +29,11 @@ def test_singleton_bins_1D_data(data, scale, translate, test_engine):
     data1 = data
     data2 = [scale * item + translate for item in data]
 
+    if np.max(data1) > 1e5:
+      data1 = [item / 1e5 for item in data1]
+    if np.max(data2) > 1e5:
+      data2 = [item / 1e5 for item in data2]
+
     # Use `fcBin`
     nBins, mx, my  = test_engine.fcBin(test_engine.convert(data1), test_engine.convert(data2),
                                          test_engine.convert([]), 1.0, nargout=3)
@@ -70,40 +75,47 @@ def test_singleton_bins_2D_data(data, scale, row, translate, test_engine):
     # Turn data into a 2D array with row length given by `row`
     data = np.array(data).reshape(-1, row)
 
+    # If data is very big scale it down
+    if np.max(data) > 1e10:
+      scale = scale / 1e10
+      translate = translate / 1e10
+
     # Use the initial data to generate two vectors worth of data
     # based on some scaling and translation to get data2
     data1 = data
     data2 = [[scale * item + translate for item in row] for row in data]
 
     # Use `fcBin`
-    nBins, mx, my  = test_engine.fcBin(matlab.double(data1), matlab.double(data2),
-                                         to_matlab_type([]), 1.0, nargout=3)
+    print("data1 = " + str(data1))
+    print("data2 = " + str(data2))
+    nBins, mx, my  = test_engine.fcBin(test_engine.convert(data1), test_engine.convert(data2),
+                                         test_engine.convert([]), 1.0, nargout=3)
 
     # Number of bins is the length of the data
     # minus the number of NaNs in combined data
     datacomb =[data1[i][j] + data2[i][j] for j in range(row) for i in range(len(data1))]
-    assert nBins == (len(datacomb) - sum([np.isnan(item) for item in datacomb]))
+    assert nBins == (len(datacomb) - sum([np.isnan(item) for item in datacomb])), f"nBins = {nBins}, datacomb = {datacomb}"
 
     # Helper routine to check results
     def check(ys, data):
       # If the result was a singleton float, it should be in the original data
       if type(ys) == float:
-        assert np.any([np.isclose(ys, item, equal_nan=True) for item in data])
+        assert np.any([np.isclose(ys, item, equal_nan=True) for item in data]), f"ys = {ys}, data = {data}"
       else:
         for bin in ys:
           # every bin is of size 1
-          assert len(bin) == 1
+          assert len(bin) == 1, f"bin = {bin}"
           # every element of the bin came originally from data
           # unless it is `inf` which seems a corner case in `fcBin` not considered
           if not(np.isinf(bin[0])):
-            assert np.any([np.isclose(bin[0], item, equal_nan=True) for item in data])
+            assert np.any([np.isclose(bin[0], item, equal_nan=True) for item in data]), f"bin = {bin}, data = {data}"
 
     check(mx, data1)
     check(my, data2)
 
 # Test fixtures for an empty dx
 test_data_dx_length_eq_zero = [
-  # # Cases with empty dx
+  # Cases with empty dx
    {"x": []
   , "y": []
   , "dx": []
@@ -111,6 +123,15 @@ test_data_dx_length_eq_zero = [
   , "mx": []
   , "my": []
   , "nBins": 0
+  },
+
+   {"x": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+  , "y": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+  , "dx": []
+  , "nPerBin": 1.0
+  , "mx": [[0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0]]
+  , "my": [[0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0]]
+  , "nBins": 11
   },
 
   {"x": [10.0, 8.0, 6.0, 4.0, 2.0]
@@ -122,14 +143,15 @@ test_data_dx_length_eq_zero = [
   , "nBins": 2
   },
 
-    {"x": [10.0, 8.0, 6.0, 4.0, 2.0]
+  {"x": [10.0, 8.0, 6.0, 4.0, 2.0]
   , "y": [0, 2.0, 4.0, 6.0, 8.0]
   , "dx": []
   , "nPerBin": 1.0
-   , "mx": [[2.0], [4.0], [6.0], [8.0], [10.0]]
-   , "my": [[8.0], [6.0], [4.0], [2.0], [0.0]]
-   , "nBins": 5
-  }]
+  , "mx": [[2.0], [4.0], [6.0], [8.0], [10.0]]
+  , "my": [[8.0], [6.0], [4.0], [2.0], [0.0]]
+  , "nBins": 5
+  }
+  ]
 
 # Test fixtures for scalar dx
 test_data_dx_length_eq_one = [
@@ -167,7 +189,7 @@ test_data_dx_length_eq_one = [
   , "mx": [[2.1000]]
   , "my": [[80.0]]
   , "nBins": 1
-   }
+  }
   ]
 
 # Test fixtures for vector dx
@@ -195,7 +217,8 @@ test_data_dx_length_gt_one = [
   , "mx": [[5.0]]
   , "my": [[99.3333]]
   , "nBins": 1
- }]
+ }
+]
 
 @pytest.mark.parametrize('data', test_data_dx_length_eq_zero +
                                  test_data_dx_length_eq_one +
@@ -205,15 +228,17 @@ def test_cpdBin_dx_sclar(test_engine, data):
     Test cpdBin with scalar dx or empty dx
     """
     # Apply the test case
-    nBins, mx, my  = test_engine.fcBin(to_matlab_type(data["x"]), to_matlab_type(data["y"]),
-                                         to_matlab_type(data["dx"]), data["nPerBin"],
-                                        nargout=3)
+    nBins, mx, my  = test_engine.fcBin(test_engine.convert(data["x"])
+                                      ,test_engine.convert(data["y"])
+                                      ,test_engine.convert(data["dx"])
+                                      ,data["nPerBin"]
+                                      ,nargout=3)
     # Check the results
     assert test_engine.equal(mx, test_engine.convert(data["mx"]))
     assert test_engine.equal(my, test_engine.convert(data["my"]))
     assert nBins == data["nBins"]
 
-# Lastly test against site-specific data
+# Lastly test against site-specific data
 def test_cpdBin_sitedata(test_engine):
     input_names  = ['x', 'y', 'dx', 'nPerBin']
     output_names = ['nBins', 'mx', 'my']
@@ -227,9 +252,9 @@ def test_cpdBin_sitedata(test_engine):
           # check if the file is zero bytes or not
           if os.path.getsize(path_to_data) != 0:
             column = pd.read_csv(path_to_data, header=None).iloc[:,:].to_numpy()
-            input_data[name] = matlab.double(column.tolist())
+            input_data[name] = test_engine.convert(column.tolist())
           else:
-            input_data[name] = matlab.double([])
+            input_data[name] = test_engine.convert([])
 
         output_data = {}
         for name in output_names:
@@ -237,14 +262,15 @@ def test_cpdBin_sitedata(test_engine):
           # check if the file is zero bytes or not
           if os.path.getsize(path_to_data) != 0:
             column = pd.read_csv(path_to_data, header=None).iloc[:,:].to_numpy()
-            output_data[name] = matlab.double(column.tolist())
+            output_data[name] = test_engine.convert(column.tolist())
           else:
-            output_data[name] = matlab.double([])
+            output_data[name] = test_engine.convert([])
 
         # Apply fcBin
-        nBins, mx, my  = test_engine.fcBin(input_data["x"], input_data["y"],
-                                      input_data["dx"], input_data["nPerBin"],
-                                      nargout=3)
+        nBins, mx, my  = test_engine.fcBin(input_data["x"]
+                                        , input_data["y"]
+                                        , input_data["dx"]
+                                        , input_data["nPerBin"], nargout=3)
 
         assert test_engine.equal(mx, test_engine.convert(output_data["mx"]))
         assert test_engine.equal(my, test_engine.convert(output_data["my"]))
