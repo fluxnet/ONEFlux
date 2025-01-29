3ca6afcf78910ecbf7fd4d22e15ce8ae73020805
Dominic Orchard <dom.orchard@gmail.com>
2025-01-29 09:44:06 +0000

    test dictionary as well as json

--- tests/unit_tests/test_ustar_cp/test_fcReadFields.py
+++ tests/unit_tests/test_ustar_cp/test_fcReadFields.py
@@ -3,8 +3,8 @@ import numpy as np
 import json
 
 @pytest.fixture
-def sample_stats_json():
-    """Create a JSON-encoded single-entry MATLAB-compatible stats struct."""
+def sample_stats():
+    """Create a stats struct."""
     stats_entry = {
         "n": 1.0, "Cp": float("nan"), "Fmax": float("nan"), "p": float("nan"),
         "b0": float("nan"), "b1": float("nan"), "b2": float("nan"), "c2": float("nan"),
@@ -13,7 +13,24 @@ def sample_stats_json():
         "ruStarVsT": float("nan"), "puStarVsT": float("nan"),
         "mT": float("nan"), "ciT": float("nan")
     }
-    return json.dumps(stats_entry, indent = 4)
+    return stats_entry
+
+@pytest.mark.parametrize(
+    "field_name, expected",
+    [
+        ("n", [[1.0]]),
+        ("Cp", [[np.nan]])
+    ]
+)
+def test_fcReadFields_valid(test_engine, sample_stats, field_name, expected):
+    """Test fcReadFields with a single-entry JSON."""
+    sample_stats_json = json.dumps(sample_stats, indent = 4)
+    result = test_engine.fcReadFields(sample_stats_json, field_name, 'jsondecode', 1)
+    assert test_engine.equal(result, test_engine.convert(expected))
+
+    result = test_engine.fcReadFields(sample_stats, field_name)
+    assert test_engine.equal(result, test_engine.convert(expected))
+
 
 @pytest.fixture
 def multi_stats_json():
@@ -30,18 +47,6 @@ def multi_stats_json():
     multi_stats = [[[stats_entry, stats_entry], [stats_entry, stats_entry]],
                    [[stats_entry, stats_entry], [stats_entry, stats_entry]]]
     return json.dumps(multi_stats, indent = 4)
-
-@pytest.mark.parametrize(
-    "field_name, expected",
-    [
-        ("n", [[1.0]]),
-        ("Cp", [[np.nan]])
-    ]
-)
-def test_fcReadFields_valid(test_engine, sample_stats_json, field_name, expected):
-    """Test fcReadFields with a single-entry JSON."""
-    result = test_engine.fcReadFields(sample_stats_json, field_name, 'jsondecode', 1)
-    assert test_engine.equal(result, test_engine.convert(expected))
 
 @pytest.mark.parametrize(
     "field_name, expected",
