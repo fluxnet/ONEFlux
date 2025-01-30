2c862fa5554bd73bdb80204cc6bfbd7a00844c7a
isaac <ia464@cam.ac.uk>
2025-01-30 16:05:49 +0000

    Remove logging from findchangepouint

--- oneflux_steps/ustar_cp_refactor_wip/cpdFindChangePoint20100901.m
+++ oneflux_steps/ustar_cp_refactor_wip/cpdFindChangePoint20100901.m
@@ -34,42 +34,26 @@
 %	=======================================================================
 %	=======================================================================
 
-	metadata = struct();
-	metadata.siteFile = 'CA-Cbo_qca_ustar_2007';
-	metadata.oneFluxDir = '/home/ia/iccs_repos/ONEFlux/';
-	metadata.relArtifactsDir = 'tests/test_artifacts';
-	metadata.frequency = 50;  %frequncy_to_log_input/ouput, defualt is 10 if not specified
 
-
-%	Initialize outputs. 
-	metadata.inputNames = {};
-	metadata.outputNames = {'Cp2','s2','Cp3','s3'};
-	[Cp2, Cp3, s2, s3] = logFuncResult('log.json', @initValues, metadata);
-	% [Cp2, Cp3, s2, s3] = initValues();
+	[Cp2, Cp3, s2, s3] = initValues();
 %	=======================================================================
 %	=======================================================================
 
 %	Exclude missing data. 
-	metadata.inputNames = {'xx', 'yy'};
-	metadata.outputNames = {'x', 'y'};
-	[x, y] = logFuncResult('log.json', @removeNans, metadata, xx, yy);
-	% [x, y] = removeNans(xx, yy);
+
+	[x, y] = removeNans(xx, yy);
 
 	n=length(x+y); if n<10; return; end;
 %	Exclude extreme lin reg outliers. 
 	
-	metadata.inputNames = {'x', 'y', 'n'};
-	metadata.outputNames = {'x', 'y'};
-	[x, y] = logFuncResult('log.json', @removeOutliers, metadata, x, y, n);
-	% [x, y] = removeOutliers(x, y, n);
+
+	[x, y] = removeOutliers(x, y, n);
 
 	n=length(x+y); if n<10; return; end;
 
 	%	Compute statistics of reduced (null hypothesis) models
 %	for later testing of Cp2 and Cp3 significance.  
-	metadata.inputNames = {'x', 'y', 'n'};
-	metadata.outputNames = {'SSERed2', 'SSERed3'};
-	% [SSERed2, SSERed3] = logFuncResult('log.json', @computeReducedModels, metadata, x, y, n);
+;
 	[SSERed2, SSERed3] = computeReducedModels(x, y, n);
 
 	nRed2=1; nRed3=2; %nRed2 and nRed3 not used,
@@ -86,17 +70,13 @@
 		% fit operational 2 parameter model, with zero slope above Cp2: 
 		% 2 connected line segments, segment 2 has zero slope
 		% parameters b0, b1 and xCp
-		metadata.inputNames = {'i', 'n', 'x', 'y', 'SSERed2', 'Fc2'};
-		metadata.outputNames = {'iAbv', 'Fc2'};
-		[iAbv, Fc2] = logFuncResult('log.json', @fitOperational2ParamModel, metadata, i, n, x, y, SSERed2, Fc2);
-		% [iAbv, Fc2] = fitOperational2ParamModel(i, n, x, y, SSERed2, Fc2); %nfull2, define within function
+
+		[iAbv, Fc2] = fitOperational2ParamModel(i, n, x, y, SSERed2, Fc2); %nfull2, define within function
 		% fit diagnostic 3 parameter model, with non-zero slope above Cp2: 
 		% 2 connected line segments with noslope constraints 
 		% parameters b0, b1, b2 and xCp
-		metadata.inputNames = {'i', 'iAbv', 'n', 'x', 'y', 'SSERed3', 'Fc3'};
-		metadata.outputNames = {'Fc3'};
-		[Fc3] = logFuncResult('log.json', @fitOperational3ParamModel, metadata, i, iAbv, n, x, y, SSERed3, Fc3);
-		% [Fc3] = fitOperational3ParamModel(i, iAbv, n, x, y, SSERed3, Fc3); %nfull3, fc3 define within function
+
+		[Fc3] = fitOperational3ParamModel(i, iAbv, n, x, y, SSERed3, Fc3); %nfull3, fc3 define within function
 
 	end; 
 	
@@ -104,32 +84,24 @@
 %	Calc stats and test for significance of Fmax scores.
 
 	pSig=0.05; 
-	metadata.inputNames = {'Fc2', 'x', 'y', 'n', 'pSig'};
-	metadata.outputNames = {'Fmax2', 'iCp2', 'xCp2', 'a2', 'a2int', 'yHat2', 'p2', 'Cp2'};
-	[Fmax2, iCp2, xCp2, a2, a2int, yHat2, p2, Cp2] = logFuncResult('log.json', @fitTwoParameterModel, metadata, Fc2, x, y, n, pSig);
-	% [Fmax2, iCp2, xCp2, a2, a2int, yHat2, p2, Cp2] = fitTwoParameterModel(Fc2, x, y, n, pSig);
 
-	metadata.inputNames = {'Fc3', 'x', 'y', 'n', 'pSig'};
-	metadata.outputNames = {'Fmax3', 'iCp3', 'xCp3', 'a3', 'a3int', 'yHat3', 'p3', 'Cp3'};
-	[Fmax3, iCp3, xCp3, a3, a3int, yHat3, p3, Cp3] = logFuncResult('log.json', @fitThreeParameterModel, metadata, Fc3, x, y, n, pSig);
-	% [Fmax3, iCp3, xCp3, a3, a3int, yHat3, p3, Cp3] = fitThreeParameterModel(Fc3, x, y, n, pSig);
+	[Fmax2, iCp2, xCp2, a2, a2int, yHat2, p2, Cp2] = fitTwoParameterModel(Fc2, x, y, n, pSig);
+
+
+	[Fmax3, iCp3, xCp3, a3, a3int, yHat3, p3, Cp3] = fitThreeParameterModel(Fc3, x, y, n, pSig);
 
 %	Assign values to s2, but only if not too close to end points.  	
 	
 	s2.n=n; s3.n=n; 
 	
 	if iCp2>(nEndPts) & iCp2<(n-nEndPts);
-		metadata.inputNames = {'a2', 'a2int', 'Cp2', 'Fmax2', 'p2', 's2'};
-		metadata.outputNames = {'s2'};
-		s2 = logFuncResult('log.json', @updateS2, metadata, a2, a2int, Cp2, Fmax2, p2, s2);
-		% [s2] = updateS2(a2, a2int, Cp2, Fmax2, p2, s2);
+
+		[s2] = updateS2(a2, a2int, Cp2, Fmax2, p2, s2);
 	end;
 	
 	if iCp3>(nEndPts) & iCp3<(n-nEndPts);
-		metadata.inputNames = {'a3', 'a3int', 'xCp3', 'Fmax3', 'p3', 's3'};
-		metadata.outputNames = {'s3'};
-		s3 = logFuncResult('log.json', @updateS3, metadata, a3, a3int, xCp3, Fmax3, p3, s3);  % typo? xCp3 or Cp3?
-		% [s3] = updateS3(a3, a3int, xCp3, Fmax3, p3, s3);
+  % typo? xCp3 or Cp3?
+		[s3] = updateS3(a3, a3int, xCp3, Fmax3, p3, s3);
 	end;
 	
 %	=======================================================================
